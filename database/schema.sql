e8d49d5ada80dec9b5d32779ca45ab9ca8e94546
annadelnegro <annaluciadelnegro@gmail.com>
2025-10-29 19:05:52 -0400

    fixed users db table

--- database/schema.sql
+++ database/schema.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS waste_not_kitchen CHARACTER SET utf8mb4 COLLATE ut
 USE waste_not_kitchen;
 
 CREATE TABLE IF NOT EXISTS users (
-id INT(11) AUTO_INCREMENT PRIMARY KEY,
-username VARCHAR(50) NOT NULL,
-email VARCHAR(100) NOT NULL,
-password VARCHAR(255) NOT NULL,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(50) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	role ENUM('admin', 'restaurant', 'customer', 'donor', 'needy') NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
