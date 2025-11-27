953bfd4b9a70685731d251312202d260e35daac3
mtthwel <m.elewis@outlook.com>
2025-11-26 19:22:11 -0500

    Matthew Branch

---
+++ modules/needy/needy-dashboard.php
@@ -0,0 +1,48 @@
+<?php
+// Waste-Not-Kitchen Needy Dashboard
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<title>Needy-Dashboard</title>
+		
+		<!-- External CSS -->
+		<link rel="stylesheet" href="../assets/css/needy-dashboard.css">
+	</head>
+	
+	<body>
+		<div class="welcome">
+			<!-- Add name to welcome -->
+			<h1> Welcome. </h1>
+		</div>
+		
+		<div class="plates">
+			<!-- Create a scroll search for available plates -->
+			<h2> Available Plates Paid By Donors</h2>
+		</div>
+		
+		<!-- Concept -->
+		<div class="plates-scroll">
+			<ul>
+				<li>Dish A</li>
+				<li>Dish B</li>
+				<li>Dish C</li>
+				<li>Dish D</li>
+				<li>Dish E</li>
+				<li>Dish F</li>
+				<li>Dish G</li>
+				<li>Dish H</li>
+				<li>Dish I</li>
+				<li>Dish J</li>
+				<li>Dish K</li>
+				<li>Dish L</li>
+				<li>Dish M</li>
+				<li>Dish N</li>
+			</ul>
+		</div>
+		
+		<a href="cart-needy.php" class="cart-button">Cart</a>
+	</body>
+</html>
